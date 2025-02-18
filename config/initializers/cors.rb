92affcc68869e99a1661b8bf1367d4f29e0ac997
Catalin Lutescu <lutescudaniel@gmail.com>
2025-02-18 14:30:19 +0200

    some fixes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,18 +5,9 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "*"
-#     resource "*",
-#       headers: :any,
-#       methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
-#   end
-# end
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001' # Adjust this to match your frontend URL
+    origins 'http://localhost:3001'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -5,10 +5,14 @@ const Dashboard = () => {
   const [monitors, setMonitors] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/monitors')
-      .then(response => setMonitors(response.data))
-      .catch(error => console.error(error));
+    axios.get('https://localhost:3000/api/url_monitors')
+      .then(response => {
+        console.log('Monitors:', response.data); // Debugging line
+        setMonitors(response.data);
+      })
+      .catch(error => console.error('Error fetching monitors:', error));
   }, []);
+
 
   return (
     <div>
@@ -16,8 +20,9 @@ const Dashboard = () => {
       {monitors.map(monitor => (
         <div key={monitor.id}>
           <h2>{monitor.name}</h2>
-          <p>{monitor.url}</p>
+          <p>URL: {monitor.url}</p>
           <p>Status: {monitor.status}</p>
+          <p>Last Checked: {new Date(monitor.last_checked_at).toLocaleString()}</p>
         </div>
       ))}
     </div>
